7371fbc8c55354f5d4fe30d8f5eb9d0e5e8a74bb
soyeon <wsy4533@naver.com>
2021-12-08 00:42:21 +0900

    Chapter3-Chapter4 02

---
+++ python_basic/myvenv/Chapter4/01.연산(1).py
@@ -0,0 +1,41 @@
+# 1. 대입연산
+# 변수이름 = 데이터
+
+# 2. 산술연산
+# - 숫자연산
+x = 5
+y = 2
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y) # 몫
+print(x % y) # 나머지
+print(x ** y) # 제곱
+
+# - 문자열 연산
+tag1 = "#파이썬"
+tag2 = "#장고"
+tag3 = "#공부"
+
+tag = tag1 + tag2 + tag3
+print(tag)
+
+message = "우린 모두 파이썬을 사랑합니다.\n" * 5
+print(message)
+
+# 복합할당연산자
+level = 10 # (레벨 1 증가)
+level += 1  # level = level + 1
+
+health = 2000 # (체력 300 감소)
+health -= 300 # heantl = health - 300
+
+attack = 300 # (공격력 1.5배 증가)
+attack *= 1.5 # attack = attack * 1.5
+
+speed = 420 # (이동속도 50% 감소)
+speed /= 2 # speed = speed / 2
+
+print(level, health, attack, speed)
